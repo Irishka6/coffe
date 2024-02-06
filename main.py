87764eede5a68493b947c1a0e1267917e293c185
Irina <yaurio@yandex.ru>
2024-02-06 17:44:04 +0500

    1

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-from get_lines import count_lines
-import argparse
-import os
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', default='', nargs='*', type=str)
-    args = parser.parse_args()
-    count_lines(args.file)
+from get_lines import print_error
 
-
-if __name__ == "__main__":
-    main()
-
-os.system("python3 get_lines.py --file gvgjvkihbh.txt")
+print_error("test message")
